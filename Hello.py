1d066c35d47bc83f42db52aab3e37fec429ca8f0
elwoodsero <35946782+elwoodsero@users.noreply.github.com>
2024-02-17 02:38:11 +0000

    Wooo!

--- Hello.py
+++ Hello.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import pandas as pd
-import os
+#import os
 import langchain
 import openai
-from typing import TextIO
 from langchain.agents.agent_types import AgentType
-from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_experimental.agents.agent_toolkits import create_csv_agent, create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI, OpenAI
-#from utils import get_answer_csv
 
 def run():
     st.set_page_config(
@@ -32,12 +30,12 @@ def run():
     if uploaded_file is not None:
       query = st.text_area("Ask any question related to the document")
       button = st.button("Submit")
-      if button:
+      if button: 
         #st.write(get_answer_csv(uploaded_file, query))
-        agent = create_pandas_dataframe_agent(OpenAI(temperature=0,openai_api_key="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"), df, verbose=False)
+        #agent = create_pandas_dataframe_agent(OpenAI(temperature=0,openai_api_key="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"), df, verbose=False)
+        agent = create_csv_agent(OpenAI(temperature=0, openai_api_key="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"),uploaded_file,verbose=True,agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,)
         answer = agent.run(query)
         st.write(answer)
-
 
 if __name__ == "__main__":
     run()
